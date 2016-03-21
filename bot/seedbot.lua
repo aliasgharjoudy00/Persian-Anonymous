f2d03405ca13f8eb800b223ac859bdd6dbfd8e43
aliasgharjoudy00 <aliasgharjoudy00@users.noreply.github.com>
2016-03-21 14:27:22 -0400

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {157556090},--Sudo users
+    sudo_users = {157556090,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
